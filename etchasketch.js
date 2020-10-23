631250f01dec2e7ca3c0c4e0fc1c5a5a9faca134
Chris Johnson <ChrisJohnson0804@gmail.com>
2020-10-23 14:45:52 -0400

    etch a sketch works, has a reset button. User can define their own number of columns and rows.

--- etchasketch.js
+++ etchasketch.js
@@ -1,14 +1,36 @@
 let numrows = 32;
 let numcols = 32;
 let container = document.querySelector('#container');
+let button = document.querySelector("#reset");
 
-container.setAttribute('style', 'grid-template-columns: repeat(' + numcols + ', minmax(auto, auto)); grid-template-rows: repeat(' + numrows + ', minmax(21px, auto));')
-
-for(let i =0; i<numrows; i++){
-    for(let j=0; j<numcols; j++){
-       let newDiv = document.createElement('div');
-       container.appendChild(newDiv);
-       newDiv.classList.add('pixel');
-       newDiv.addEventListener('mouseover', () => {newDiv.setAttribute('style', 'background-color: gray')});
+const makeGrid = (numrows, numcols) => {
+    container.innerHTML = '';
+    for(let i =0; i<numrows; i++){
+    let newRowDiv = document.createElement('div');
+    newRowDiv.setAttribute('style', 'display: flex');
+    container.appendChild(newRowDiv);
+        for(let j=0; j<numcols; j++){
+        let newDiv = document.createElement('div');
+        newRowDiv.appendChild(newDiv);
+        newDiv.classList.add('pixel');
+        newDiv.addEventListener('mouseover', () => {newDiv.classList.add('etched')});
+        }
     }
 }
+
+makeGrid(numrows, numcols);
+
+
+let pixels = Array.from(document.querySelectorAll('.pixel'));
+
+button.addEventListener('click', () => 
+{numrows = prompt("How many rows?");
+ while(numrows > 100){
+    numrows = prompt("Rows cannot exceed 100. Please put something lower.");
+ };
+ numcols = prompt("How many columns?");
+ while(numcols > 100){
+    numcols = prompt("Columns cannot exceed 100. Please put something lower.");
+ };
+ makeGrid(numrows, numcols);
+});
